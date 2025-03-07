1e57c17875aaadea0ec4ae1adf9d12fa9e1df8e5
Molly Rose <molly@mollymorphous.dev>
2025-03-07 09:18:15 -0500

    API config (#24)
    Create a config system that uses
    [pydantic-settings](https://docs.pydantic.dev/2.10/api/pydantic_settings/)
    to load settings from a `config.toml` file, environment variables, and
    Docker secrets. Dev configs are copied into the containers, but the prod
    config is the default.
    
    I also changed to a sync+restart strategy for watching in Docker Compose
    because Granian doesn't reload if the app fails to instantiate because a
    synced file has an error.

---
+++ api/app/config.py
@@ -0,0 +1,101 @@
+from __future__ import annotations
+
+from enum import StrEnum, auto
+from pathlib import Path
+
+import tomllib
+from pydantic import BaseModel, Field
+from pydantic_settings import (
+    BaseSettings,
+    PydanticBaseSettingsSource,
+    SettingsConfigDict,
+    TomlConfigSettingsSource,
+)
+
+
+def load_pyproject_version() -> str | None:
+    """Load version number from pyproject.toml"""
+
+    path = Path(__file__).parents[1] / "pyproject.toml"
+    if path.exists():
+        with path.open("rb") as file:
+            project = tomllib.load(file)["project"]
+            if project is not None:
+                return project["version"]
+
+    return None
+
+
+class BuildConfig(BaseModel):
+    """Config storing info on this build"""
+
+    version: str = Field(default_factory=load_pyproject_version)
+
+
+class DebugConfig(BaseModel):
+    """App debugging config"""
+
+    stack_traces: bool = Field(False)
+    """Expose stack traces to the client for exceptions resulting in 500 errors."""
+
+
+class LogLevel(StrEnum):
+    """Log levels matched to the logging module's standard log level names"""
+
+    @staticmethod
+    def _generate_next_value_(name, *args):
+        """Keep names uppercase to match what the logging module expects"""
+        return name.upper()
+
+    CRITICAL = auto()
+    ERROR = auto()
+    WARNING = auto()
+    INFO = auto()
+    DEBUG = auto()
+
+
+class LogConfig(BaseModel):
+    """Config for the logging module, including log level and format"""
+
+    level: LogLevel = Field(LogLevel.WARNING)
+    format: str = Field("[{levelname}] {message} ({filename}:{lineno})")
+
+
+class Config(BaseSettings):
+    """
+    App-wide config, loaded from `config.toml` in the working directory, environment variables,
+    `.env` files, and Docker secrets.
+    """
+
+    build: BuildConfig = Field(default_factory=BuildConfig)
+    debug: DebugConfig = Field(DebugConfig())
+    log: LogConfig = Field(LogConfig())
+
+    model_config = SettingsConfigDict(
+        env_prefix="CHIPMUNKS_",
+        env_nested_delimiter="_",
+        env_nested_max_split=1,
+        toml_file="config.toml",
+    )
+
+    @classmethod
+    def settings_customise_sources(
+        cls,
+        settings_cls: type[BaseSettings],
+        init_settings: PydanticBaseSettingsSource,
+        env_settings: PydanticBaseSettingsSource,
+        dotenv_settings: PydanticBaseSettingsSource,
+        file_secret_settings: PydanticBaseSettingsSource,
+    ) -> tuple[PydanticBaseSettingsSource, ...]:
+        return (
+            init_settings,
+            TomlConfigSettingsSource(settings_cls),
+            env_settings,
+            dotenv_settings,
+            file_secret_settings,
+        )
+
+
+# For debugging, execute this file to dump the resolved config as JSON
+if __name__ == "__main__":
+    print(Config().model_dump_json(indent=2))
--- api/app/main.py
+++ api/app/main.py
@@ -1,6 +1,15 @@
+import logging
+
 from fastapi import FastAPI
 
-app = FastAPI(title="Chipmunks")
+from .config import Config
+
+# Set up app instance
+config = Config()
+logging.basicConfig(level=config.log.level, format=config.log.format, style="{")
+app = FastAPI(
+    title="Chipmunks", debug=config.debug.stack_traces, version=config.build.version
+)
 
 
 @app.get("/")
